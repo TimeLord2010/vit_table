ed04b3beab2b474a4ec9b44fd255cd5974809eaf
Vinicius <timelord1025@gmail.com>
2024-08-16 12:07:19 -0300

    fix: imports

--- lib/vit_table.dart
+++ lib/vit_table.dart
@@ -1,1 +1,6 @@
 library vit_table;
+
+export 'package:vit_table/data/models/vit_table_column.dart';
+export 'package:vit_table/data/models/vit_table_row.dart';
+export 'package:vit_table/ui/theme/vit_table_style.dart';
+export 'package:vit_table/vit_table.dart';
