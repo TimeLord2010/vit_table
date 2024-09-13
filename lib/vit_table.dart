f9fc3092bd4fa97c8d294db79f5c8e15bebedfb8
Vinicius <timelord1025@gmail.com>
2024-09-13 16:00:40 -0300

    feat: VitTableTheme

--- lib/vit_table.dart
+++ lib/vit_table.dart
@@ -4,3 +4,4 @@ export 'package:vit_table/data/models/vit_table_column.dart';
 export 'package:vit_table/data/models/vit_table_row.dart';
 export 'package:vit_table/ui/components/organisms/vit_table.dart';
 export 'package:vit_table/ui/theme/vit_table_style.dart';
+export 'package:vit_table/ui/theme/vit_table_theme.dart';
